3120d8060f82777a231c6aee74b8a9cb40eed50d
Randall Randall <randall@randallsquared.com>
2018-01-31 20:50:21 -0500

    2.0: stop defeating micro-dev's hot reloading

--- README.md
+++ README.md
@@ -4,11 +4,22 @@ A nano router for Zeit's [micro](https://github.com/zeit/micro)
 
 ```
 const micro = require('micro');
-const { get, post, route } = require('swarfless');
+const Swarfless = require('swarfless');
+const r = new Swarfless();
 
 
-get('/', async () => 'Root of server');
+r.get('/', async () => 'Root of server');
+
+// or
+const { get, post } = r;
+
 get('/hello/:name', async (req) => `Hi, ${req.params.name}`);
+post('/set-name/:name', async (req) => `Setting name to ${req.params.name}`);
 
-module.exports = route;
+module.exports = r.get_route_handler();
 ```
+
+## Changes
+
+Version 2 has changes to avoid storing the route in the module itself, which interferes with hot-reloading from micro-dev.  Due to those changes, you must now create a Swarfless instance before using the route-building methods.
+
--- index.js
+++ index.js
@@ -2,104 +2,94 @@ const urlparse = require('url').parse;
 const matcher = require('path-match')();
 const Boom = require('boom');
 
-class Router {}
-
-const routes = {
-  post: [],
-  get: [],
-  put: [],
-  del: [],
-  options: [],
-  head: [],
-  _mid: [],
+const MIDDLEWARE = '_mid';
+const METHODS = {
+  get: 'GET',
+  post: 'POST',
+  put: 'PUT',
+  opt: 'OPTIONS',
+  options: 'OPTIONS',
+  del: 'DELETE',
+  delete: 'DELETE',
+  head: 'HEAD',
+  use: MIDDLEWARE,
+  mid: MIDDLEWARE,
 };
 
-const add_route = (method, path, handler) => {
-  const rank = path.split('/').length;
-  const match = matcher(path);
-  const pack = {
-    rank,
-    match,
-    handler
-  };
-  routes[method].push(pack);
-  routes[method].sort((a, b) => b.rank - a.rank);
-};
+class Router {
 
-const post = (path, handler) => {
-  add_route('post', path, handler);
-};
-
-const get = (path, handler) => {
-  add_route('get', path, handler);
-};
-
-const put = (path, handler) => {
-  add_route('put', path, handler);
-};
-
-const del = (path, handler) => {
-  add_route('del', path, handler);
-};
-
-const options = (path, handler) => {
-  add_route('options', path, handler);
-};
-
-const head = (path, handler) => {
-  add_route('head', path, handler);
-};
-
-const mid = (path, handler) => {
-  add_route('_mid', path, handler);
-};
-
-const check_handler = (handler_pack, req) => {
-  const {
-    match,
-    handler
-  } = handler_pack;
-  const params = match(urlparse(req.url).pathname);
-
-  if (params === false) return undefined;
-
-  if (req.params) req.params_history.push(req.params);
-  req.params = params;
-  return handler;
-};
-
-const route = async(req, res) => {
-  const packs = routes[req.method.toLowerCase()];
-  for (const pack of packs) {
-    req.handler = req.handler || check_handler(pack, req);
-  }
-
-  req.params_history = [];
-  if (!res.finished) {
-    // check middleware backwards, because '/' is before '/something'
-    const packs = routes['_mid'];
-    let left = packs.length;
-    while (left--) {
-      const middleware = check_handler(packs[left], req);
-      if (middleware) await middleware(req, res);
+  constructor() {
+    this.routes = {};
+    for (const [name, builder] of this.get_builders()) {
+      this[name] = builder;
     }
   }
-  if (res.finished) return void null;
-  if (req.handler) return req.handler(req, res);
 
-  throw Boom.notFound('Not found');
+  // this is separate from get_builders because it might
+  // be used explicitly by Router users for methods not
+  // already given, like PATCH
+  get_builder(given_method) {
+    const method = given_method.toLowerCase();
+    return (path, handler) => {
+      this.add_route(method, path, handler);
+    };
+  }
 
-};
+  get_builders() {
+    return Object
+      .entries(METHODS)
+      .map(([name, method]) => [name, this.get_builder(method)]);
+  }
 
-Router.get = get;
-Router.post = post;
-Router.put = put;
-Router.del = del;
-Router.options = options;
-Router.head = head;
-Router.mid = mid;
-Router.route = route;
-Router.Router = Router;
+  get_route_handler() {
+    return this.route.bind(this);
+  }
+
+  add_route(method, path, handler) {
+    const rank = path.split('/').length;
+    const match = matcher(path);
+    const pack = { rank, match, handler };
+    if (!(method in this.routes)) this.routes[method] = [];
+    this.routes[method].push(pack);
+    this.routes[method].sort((a, b) => b.rank - a.rank);
+  }
+
+  check_handler(handler_pack, req) {
+    const { match, handler } = handler_pack;
+    const params = match(urlparse(req.url).pathname);
+
+    if (params === false) return undefined;
+
+    if (req.params) req.params_history.push(req.params);
+    req.params = params;
+    return handler;
+  }
+
+  async route (req, res) {
+    const packs = this.routes[req.method.toLowerCase()] || [];
+    for (const pack of packs) {
+      const handler = this.check_handler(pack, req);
+      if (!handler) continue;
+      req.handler = handler;
+      break;
+    }
+
+    req.params_history = [];
+    if (!res.finished) {
+      // check middleware backwards, because '/' is before '/something'
+      const packs = this.routes[MIDDLEWARE] || [];
+      let left = packs.length;
+      while (left--) {
+        const middleware = this.check_handler(packs[left], req);
+        if (middleware) await middleware(req, res);
+      }
+    }
+    if (res.finished) return void null;
+    if (req.handler) return req.handler(req, res);
+
+    throw Boom.notFound('Not found');
+  }
+}
 
 module.exports = Router;
 
